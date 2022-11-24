c7fe7b79d7305ac6448bfcc4fe4c79324997f9dd
Aska0526 <57811518+Aska0526@users.noreply.github.com>
2022-11-24 20:20:34 +0000

    newest version

--- fringe_analysis.py
+++ fringe_analysis.py
@@ -12,15 +12,36 @@ import os
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 
-
+#%%
 def Fourier_Transform(image):
     fft_image = fft2(image)
     return fft_image
 
-def plot_spectrum(im_fft,ax):
-    # A logarithmic colormap
-    ax.imshow(np.log10(np.abs(im_fft)))
-    #plt.colorbar()
+def Beam(x,y,x0,y0,std,E0):
+    return (E0*np.exp(-((x-x0)**2+(y-y0)**2)/(2*std**2)))**2
+
+def Interference(Beam1,Beam2,delta):
+    return Beam1+Beam2+2*np.sqrt(Beam1*Beam2)*np.cos(delta)
+    
+    
+def phase_diff(x,y,x0,y0,x1,y1,lambda_):
+    return 2*np.pi/lambda_*(np.sqrt((x-x0)**2+(y-y0)**2)-np.sqrt((x-x1)**2+(y-y1)**2))
+
+    
+x = np.linspace(0,999,1000)
+y = np.linspace(0,999,1000)
+x, y = np.meshgrid(x,y)
+z1 = Beam(x,y,700,300,200,100)
+z2 = Beam(x,y,700,300,200,100)
+delta = phase_diff(x,y,700,300,700,300,150)
+
+plt.imshow(Interference(z1,z2,delta))
+
+
+
+
+#%%
+
 
 folder = 'fringes&visib/'  #paths to folder containing images
 images = []
@@ -50,3 +71,11 @@ for i in range(6):
 plt.tight_layout()
 plt.show()
 
+#%%
+trial_img = Interference(z1,z2,delta)
+fft_trial = Fourier_Transform(trial_img)
+fft_trial = fftshift(fft_trial)
+fig = plt.figure()
+ax = ax = fig.add_axes((0,0,10,10),projection='3d')
+ax.plot_surface(x,y,np.log10(abs(fft_trial)),cmap=cm.coolwarm)
+plt.show()
