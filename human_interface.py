63dc6763938100402f58eb7ef3117481039705df
Aska0526 <57811518+Aska0526@users.noreply.github.com>
2023-03-02 16:47:19 +0000

    update human interface

--- human_interface.py
+++ human_interface.py
@@ -11,13 +11,25 @@ from fringe_analysis import Cal_Visib
 from camera_reader import Get_image
 
 cap = cv2.VideoCapture(0)
+
+count = 0
+font = cv2.FONT_HERSHEY_SIMPLEX
+visib = '0'
 while True:
-    img = Get_image(cap)
+
     keypressed = cv2.waitKey(30)
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    print(Cal_Visib(img))
-    cv2.putText(img, str(round(Cal_Visib(img),4)), (32,64), font, 0.4, (255, 255, 255), 2, cv2.LINE_AA)
-    cv2.imshow('test', img)
+    ret,frame = cap.read()
+    if count  == 30:
+        img = Get_image(cap)
+        visib = str(round(Cal_Visib(img),4)) 
+        count = 1
+    else:
+        count += 1
+        
+    cv2.putText(frame, visib, 
+                (32,64), font, 2, (255, 255, 255), 2, cv2.LINE_AA)    
+    cv2.imshow('test', frame)
     if keypressed == ord('q'):
         break
    
+    
